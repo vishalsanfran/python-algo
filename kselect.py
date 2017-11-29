4366ab110d909917d9f71af1f413cfeeba0f1938
vishalsanfran <vkhawarey@maprtech.com>
2017-11-28 21:50:10 -0800

    quick sort

--- kselect.py
+++ kselect.py
@@ -13,3 +13,13 @@ nums = [4,55,-3,90,-200,33,0,27]
 print nums
 for i in range(len(nums)):
     print("idx {} num {}".format(i, kselect(nums, i)))
+
+def qsort(nums):
+    if len(nums) < 2:
+        return nums
+    pvt = nums[len(nums)/2]
+    less = [num for num in nums if num < pvt]
+    more = [num for num in nums if num > pvt]
+    eq = [num for num in nums if num == pvt]
+    return qsort(less) + eq + qsort(more)
+print("sorted {}".format(qsort(nums)))
